d659a885cae77b493cd48af889d2b91a2e810d34
TinoMaster <oscarmarcos2673@gmail.com>
2023-03-10 17:40:03 -0500

    commit before mi first push

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,9 +1,7 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const CategoryController = require("../controllers/categories.controllers");
 
-router.get("/categories", (req, res) => {
-  console.log("siii");
-  res.json({ entra: "siiii" });
-});
+router.get("/categories", CategoryController.get_all);
 
 module.exports = router;
